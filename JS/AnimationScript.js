aa8e045c550573dd0c0b51a09f896d5a99cf42bb
devang <devangsaklani@gmail.com>
2021-06-19 09:06:33 +0530

    v1.0.1 with SwiperJS

--- JS/AnimationScript.js
+++ JS/AnimationScript.js
@@ -1,13 +1,12 @@
 //first
-const firstTL = anime.timeline({
-  duration: 1000 / 8,
-  complete: function () {
-    firstTL.restart();
-  },
-  easing: "easeOutSine",
-});
-
 const animateFirst = () => {
+  const firstTL = anime.timeline({
+    duration: 1000 / 8,
+    complete: function () {
+      firstTL.restart();
+    },
+    easing: "easeOutSine",
+  });
   let elements = document.querySelectorAll(".first-circles");
   for (let el of elements) {
     firstTL.add({
@@ -25,7 +24,6 @@ const animateFirst = () => {
     });
   }
 };
-window.addEventListener("load", animateFirst());
 
 //second
 const animateSecond = () => {
@@ -38,7 +36,6 @@ const animateSecond = () => {
     duration: 1200,
   });
 };
-window.addEventListener("load", animateSecond);
 
 //third
 const animateThird = () => {
@@ -46,7 +43,7 @@ const animateThird = () => {
   let curEl = elements[0];
   anime({
     targets: curEl,
-    translateX: 190,
+    translateX: 170,
     // translateY: [0, -50 , 0],
     loop: true,
     duration: 700,
@@ -62,7 +59,6 @@ const animateThird = () => {
     duration: 700,
   });
 };
-window.addEventListener("load", animateThird);
 
 //fourth
 const animateFourth = () => {
@@ -86,18 +82,16 @@ const animateFourth = () => {
     duration: 1000,
   });
 };
-window.addEventListener("load", animateFourth);
 
 //fifth
-const fifthTL = anime.timeline({
-  duration: 3000,
-  complete: function () {
-    fifthTL.restart();
-  },
-  easing: "easeOutSine",
-});
-
 const animateFifth = () => {
+  const fifthTL = anime.timeline({
+    duration: 3000,
+    complete: function () {
+      fifthTL.restart();
+    },
+    easing: "easeOutSine",
+  });
   let elements = document.querySelectorAll(".fifth-circles");
   for (let el of elements) {
     fifthTL.add({
@@ -124,10 +118,8 @@ const animateFifth = () => {
     });
   }
 };
-window.addEventListener("load", animateFifth());
 
 //sixth
-
 const animateSixth = () => {
   const sixthTL = anime.timeline({
     loop: true,
@@ -167,7 +159,6 @@ const animateSixth = () => {
       },
       0
     );
-    
 
   //second frame
   sixthTL.add({
@@ -176,7 +167,6 @@ const animateSixth = () => {
     rotate: 135,
     endDelay: 200,
   });
-
 
   //third frame
   sixthTL
@@ -195,7 +185,6 @@ const animateSixth = () => {
       "-=500"
     );
 
-
   //fourth frame
   sixthTL
     .add({
@@ -211,7 +200,6 @@ const animateSixth = () => {
       },
       "-=500"
     );
-
 
   //fifth frame
   sixthTL
@@ -233,7 +221,6 @@ const animateSixth = () => {
       "-=500"
     );
 
-
   //sixth frame
   sixthTL
     .add({
@@ -251,7 +238,6 @@ const animateSixth = () => {
       "-=500"
     );
 
-
   //seventh frame
   sixthTL.add({
     targets: "#sixthSVG",
@@ -260,7 +246,6 @@ const animateSixth = () => {
     endDelay: 200,
   });
 
-  
   //eighth frame
   sixthTL
     .add({
@@ -294,4 +279,96 @@ const animateSixth = () => {
       "-=500"
     );
 };
-window.addEventListener("load", animateSixth());
+
+const animateSeventh = () =>{
+  let tl = anime.timeline({
+    loop: true,
+    easing: "easeOutQuad",
+  });
+
+  tl.add({
+    targets: ".circle",
+    height: 35,
+    duration: 20,
+  })
+    .add({
+      targets: ".circle",
+      translateY: -290,
+      easing: "easeOutQuad",
+      duration: 500,
+    })
+    .add(
+      {
+        targets: ".circle",
+        height: 70,
+        duration: 300,
+      },
+      "-=500"
+    )
+    .add(
+      {
+        targets: ".circle",
+        width: [70, 35, 70],
+        duration: 300,
+      },
+      "-=500"
+    )
+    .add({
+      targets: ".circle",
+      translateY: 0,
+      duration: 500,
+      easing: "easeInQuad",
+    })
+    .add(
+      {
+        targets: ".circle",
+        width: [70, 55],
+        duration: 500,
+      },
+      "-=500"
+    )
+    .add({
+      targets: ".circle",
+      height: 35,
+      width: 70,
+      duration: 30,
+    });
+
+  //1150ms
+
+  let tlRect = anime.timeline({
+    loop: true,
+    easing: "easeOutQuad",
+  });
+
+  tlRect
+    .add({
+      targets: ".rect",
+      translateY: -180,
+      rotate: 180,
+      width: 90,
+      height: 90,
+      duration: 500,
+      delay: 10,
+    })
+    .add({
+      targets: ".rect",
+      translateY: 0,
+      duration: 500,
+      easing: "easeInQuad",
+    })
+    .add(
+      {
+        targets: ".rect",
+        width: 140,
+        height: 40,
+        duration: 500,
+        easing: "easeInQuad",
+      },
+      "-=500"
+    )
+    .add({
+      targets: ".rect",
+      duration: 40,
+    });
+}
--- JS/main.js
+++ JS/main.js
@@ -1,7 +1,7 @@
-if (localStorage.getItem("theme") == "stroked"){
-    setTheme("stroked");
-}else{
-    setTheme("filled");
+if (localStorage.getItem("theme") == "stroked") {
+  setTheme("stroked");
+} else {
+  setTheme("filled");
 }
 
 function toggleTheme() {
@@ -10,9 +10,40 @@ function toggleTheme() {
   } else {
     setTheme("stroked");
   }
+  console.log(swiper.activeIndex);
 }
 
 function setTheme(themeName) {
   localStorage.setItem("theme", themeName);
-document.body.className = themeName;
+  document.body.className = themeName;
 }
+
+const swiper = new Swiper(".swiper-container", {
+  direction: "horizontal",
+  loop: false,
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+swiper.on("slideChange", function () {
+//   console.log(swiper.activeIndex);
+});
+window.addEventListener("load", () => {
+  animateFirst();
+
+  animateSecond();
+
+  animateThird();
+
+  animateFourth();
+
+  animateFifth();
+
+  animateSixth();
+
+  animateSeventh()
+});
